f44b57cd54109194f68ac5a5c966d9548e998776
Justin Klitgaard <jlke45@gmial.com>
2024-08-10 15:07:26 +0900

    Order query results by ID, ascending

--- database/queries.js
+++ database/queries.js
@@ -1,8 +1,12 @@
 const pool = require("./pool.js");
 
 const getCourses = async () => {
-  const { rows } = await pool.query("SELECT * FROM courses");
+  const { rows } = await pool.query("SELECT * FROM courses ORDER BY id ASC");
   return rows;
+};
+
+const addCourse = async (course) => {
+  await pool.query("INSERT INTO courses (course_name) VALUES ($1)", [course]);
 };
 
 module.exports = { getCourses };
