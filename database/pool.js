fa2b53ccbd53f363978f2b5366e891df51f7a1e3
Justin Klitgaard <jlke45@gmail.com>
2024-09-03 20:52:45 +0900

    Update connection information.

--- database/pool.js
+++ database/pool.js
@@ -1,9 +1,5 @@
 const { Pool } = require("pg");
 
 module.exports = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.USER,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.PGHOST,
 });
