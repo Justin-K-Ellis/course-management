d1b38a4f1cf5a12793d0f39acabddcfde3c3f5b4
Justin Klitgaard <jlke45@gmail.com>
2024-09-04 09:19:03 +0900

    Update port config.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
 const router = require("./router.js");
 
 const app = express();
